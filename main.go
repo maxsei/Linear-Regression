726cf467dd9bf3af61ea499a83c8f596f5f559c0
Maximillian Schulte <maximilliangschulte@gmail.com>
2019-04-30 21:40:29 -0500

    got some matrix stuff to work

--- main.go
+++ main.go
@@ -51,12 +51,14 @@ func main() {
 	fmt.Println("Starting Regression")
 	m, b := newtonsRegression(X, Y, *epsilon, *iterationsVisible)
 	r := correlationCoefficient(X, Y)
+	pc := pearsonCoefficient(X, Y)
 
 	fmt.Printf(
 		"\nRegression Line: y = %.8fx + %.8f\n"+
 			"Correlation Coefficient: %.8f\n"+
+			"Pearson Coefficient: %.8f\n"+
 			"MAE: %.8f\n\n",
-		m, b, r, calcMAE(X, Y, m, b),
+		m, b, r, pc, calcMAE(X, Y, m, b),
 	)
 
 	// make XY pairs for original data as well data points created from the
@@ -84,4 +86,44 @@ func calcMAE(X, Y []float64, m, b float64) (mAE float64) {
 // predict will predict the y value for the associated x values
 func predict(m, x, b float64) float64 {
 	return m*x + b
+}
+
+// correlationCoefficient returns the correlation coefficient of two equally
+// sized vectors using the following formula:
+//
+//           nsum(xy)-sum(x)sum(y)
+//    _______________________________________
+//   ________________________________________
+// \/ [nsum(x^2)-sum(x)^2][nsum(y^2)-sum(y)^2]
+func correlationCoefficient(X, Y []float64) float64 {
+	var sumX2, sumY2, sumXY, sumX, sumY float64
+	for i := range X {
+		sumX2 += math.Pow(X[i], 2)
+		sumY2 += math.Pow(Y[i], 2)
+		sumXY += X[i] * Y[i]
+		sumX += X[i]
+		sumY += Y[i]
+	}
+	n := float64(len(X))
+	return (n*sumXY - sumX*sumY) /
+		math.Pow((n*sumX2-math.Pow(sumX, 2))*(n*sumY2-math.Pow(sumY, 2)), .5)
+}
+
+// pearsonCoefficient will calculate the pearson coefficient of the dataset
+func pearsonCoefficient(X, Y []float64) float64 {
+	var sumXMeanDiff, sumYMeanDiff float64
+	var meanX, meanY, sdX, sdY float64
+	for i := range X {
+		meanX += X[i] / float64(len(X))
+		meanY += Y[i] / float64(len(Y))
+	}
+	for i := range X {
+		sumXMeanDiff += X[i] - meanX
+		sumYMeanDiff += Y[i] - meanY
+		sdX += math.Pow(X[i]-meanX, 2)
+		sdY += math.Pow(Y[i]-meanY, 2)
+	}
+	sdX = math.Pow(sdX, .5)
+	sdX = math.Pow(sdY, .5)
+	return sumXMeanDiff * sumYMeanDiff / (sdX * sdY)
 }
